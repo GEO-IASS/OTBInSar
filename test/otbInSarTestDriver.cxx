e2fe18096e27bd8177b71a5f831cdf2f180884e8
Christophe Palmann <christophe.palmann@c-s.fr>
2016-06-10 17:15:25 +0200

    Added unit test otbHorizontalVerticalBaselineFunctorTest

--- test/otbInSarTestDriver.cxx
+++ test/otbInSarTestDriver.cxx
@@ -4,6 +4,7 @@ void RegisterTests()
   REGISTER_TEST(otbBaselineCalculatorNew);
   REGISTER_TEST(otbBaselineFunctorBaseNew);
   REGISTER_TEST(otbHorizontalVerticalBaselineFunctorNew);
+  REGISTER_TEST(otbHorizontalVerticalBaselineFunctorTest);
   REGISTER_TEST(otbLengthOrientationBaselineFunctorNew);
   REGISTER_TEST(otbParallelPerpendicularBaselineFunctorNew);
   REGISTER_TEST(otbPlatformPositionToBaselineCalculatorNew);
