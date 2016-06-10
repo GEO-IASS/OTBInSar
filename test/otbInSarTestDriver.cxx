038c27c849b065be3cca0cb18e2f7ff777aafe5b
Christophe Palmann <christophe.palmann@c-s.fr>
2016-06-10 15:57:58 +0200

    Added some unit tests

--- test/otbInSarTestDriver.cxx
+++ test/otbInSarTestDriver.cxx
@@ -2,9 +2,9 @@
 void RegisterTests()
 {
   REGISTER_TEST(otbBaselineCalculatorNew);
-  //REGISTER_TEST(otbBaselineFunctorBaseNew);
-  //REGISTER_TEST(otbHorizontalVerticalBaselineFunctorNew);
-  /*REGISTER_TEST(otbLengthOrientationBaselineFunctorNew);
+  REGISTER_TEST(otbBaselineFunctorBaseNew);
+  REGISTER_TEST(otbHorizontalVerticalBaselineFunctorNew);
+  REGISTER_TEST(otbLengthOrientationBaselineFunctorNew);
   REGISTER_TEST(otbParallelPerpendicularBaselineFunctorNew);
-  REGISTER_TEST(otbPlatformPositionToBaselineCalculatorNew);*/
+  REGISTER_TEST(otbPlatformPositionToBaselineCalculatorNew);
 }
